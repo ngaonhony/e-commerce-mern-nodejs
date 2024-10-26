33f997fa7aacf1ad2e84fddf4afd7d3990b9baba
Anh Dự Dương <Duonganhdujf5@gmail.com>
2024-10-26 12:32:20 +0700

    +

--- Frontend/src/components/ProductCard.js
+++ Frontend/src/components/ProductCard.js
@@ -20,82 +20,28 @@ const ProductCard = (props) => {
   console.log(data);
   const location = useLocation();
 
-  const wishlistState = useSelector((state) => state?.auth?.wishlist?.wishlist);
-
-  const [wishlist, setWishlist] = useState(wishlistState || []);
-
-  useEffect(() => {
-    setWishlist(wishlistState || []);
-  }, [wishlistState]);
-
-  const isProductInWishlist = (productId) => {
-    return wishlist?.some((item) => item._id === productId);
-  };
-
-  const addToWish = (productId) => {
-    if (isProductInWishlist(productId)) {
-      dispatch(addToWishlist(productId)); // Dispatch the action to update the wishlist in Redux store
-
-      const updatedWishlist = wishlist.filter((item) => item._id !== productId);
-      setWishlist(updatedWishlist);
-    } else {
-      dispatch(addToWishlist(productId)); // Dispatch the action to update the wishlist in Redux store
-
-      const product = data.find((item) => item._id === productId);
-      setWishlist([...wishlist, product]);
-    }
-  };
 
   return (
-    <>
-      {data?.map((item, index) => {
-        const isWishlist = isProductInWishlist(item._id);
-        console.log(isWishlist);
-        return (
-          <div
-            key={index}
-            className={` ${
-              location.pathname == "/product" ? `gr-${grid}` : "col-3"
-            } `}
-          >
-            <div className="product-card position-relative">
-              <div className="wishlist-icon position-absolute">
-                <button
-                  className="border-0 bg-transparent"
-                  onClick={(e) => addToWish(item?._id)}
-                >
-                  {isWishlist ? (
-                    <AiFillHeart className="fs-5 me-1" />
-                  ) : (
-                    <AiOutlineHeart className="fs-5 me-1" />
-                  )}
-                </button>
-              </div>
-
-              <div className="product-image">
+    <div className="2xl:container 2xl:mx-auto">
+      <div className="grid lg:grid-cols-4 sm:grid-cols-5 grid-cols-1 lg:gap-y-16 lg:gap-x-12 sm:gap-y-12 sm:gap-x-8 gap-y-8  p-8 ">
+        {data?.map((item, index) => {
+          return (
+            <div key={index} className="relative group product-item p-4 ">
+              {/* Hình ảnh sản phẩm */}
+              <div className="relative group overflow-hidden h-[300px] w-full "> 
+                {/* Hình ảnh sản phẩm */}
                 <img
+                  className=""
                   src={item?.images[0]?.url}
-                  // className="img-fluid d"
                   alt="product image"
-                  height={"250px"}
-                  width={"100%"}
-                  onClick={() => navigate("/product/" + item?._id)}
-                />
-                <img
-                  src={item?.images[0]?.url}
-                  // className="img-fluid d"
-                  alt="product image"
-                  height={"250px"}
-                  width={"100%"}
                   onClick={() => navigate("/product/" + item?._id)}
                 />
               </div>
-              <div className="product-details">
-                <h6 className="brand">{item?.brand}</h6>
-                <h5 className="product-title">
-                  {grid === 12 || grid === 6
-                    ? item?.title
-                    : item?.title?.substr(0, 80) + "..."}
+    
+              {/* Thông tin sản phẩm */}
+              <div className="product-details mt-4">
+                <h5 className="font-normal text-xl leading-5 text-gray-800 md:mt-6 mt-4">
+                  {grid === 12 || grid === 6 ? item?.title : item?.title?.substr(0, 80) + "..."}
                 </h5>
                 <ReactStars
                   count={5}
@@ -104,33 +50,14 @@ const ProductCard = (props) => {
                   edit={false}
                   activeColor="#ffd700"
                 />
-
-                <p className="price">Rs.{item?.price}</p>
-              </div>
-              <div className="action-bar position-absolute">
-                <div className="d-flex flex-column gap-15">
-                  {/* <button className="border-0 bg-transparent">
-                    <img src={prodcompare} alt="compare" />
-                  </button> */}
-
-                  {/* <button className="border-0 bg-transparent">
-                    <img
-                      onClick={() => navigate("/product/" + item?._id)}
-                      src={view}
-                      alt="view"
-                    />
-                  </button> */}
-                  {/* <button className="border-0 bg-transparent">
-                    <img src={addcart} alt="addcart" />
-                  </button> */}
-                </div>
+                <p className="font-semibold text-xl leading-5 text-gray-800 mt-4">Rs.{item?.price}</p>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </>
-  );
+          );
+        })}
+      </div>
+    </div>
+  );  
 };
 
 export default ProductCard;
