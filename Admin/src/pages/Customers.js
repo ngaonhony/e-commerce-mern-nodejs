5621f83c8f955a4c60f8497c25a42616fcf194ef
Anh Dự Dương <Duonganhdujf5@gmail.com>
2024-11-20 16:47:36 +0700

    Fix code như cái nùi giẻ của th Quí

--- Admin/src/pages/Customers.js
+++ Admin/src/pages/Customers.js
@@ -2,13 +2,16 @@ import React, { useEffect } from "react";
 import { Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../features/cutomers/customerSlice";
+import * as XLSX from "xlsx"; // Import thư viện xlsx
+import { saveAs } from "file-saver"; // Import thư viện file-saver
+
 const columns = [
   {
-    title: "SNo",
+    title: "Số thứ tự",
     dataIndex: "key",
   },
   {
-    title: "Name",
+    title: "Họ và tên",
     dataIndex: "name",
     sorter: (a, b) => a.name.length - b.name.length,
   },
@@ -17,32 +20,57 @@ const columns = [
     dataIndex: "email",
   },
   {
-    title: "Mobile",
+    title: "Số điện thoại",
     dataIndex: "mobile",
   },
 ];
 
 const Customers = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getUsers());
-  }, []);
+  }, [dispatch]);
+
   const customerstate = useSelector((state) => state.customer.customers);
-  const data1 = [];
-  for (let i = 0; i < customerstate.length; i++) {
-    if (customerstate[i].role !== "admin") {
-      data1.push({
-        key: i + 1,
-        name: customerstate[i].firstname + " " + customerstate[i].lastname,
-        email: customerstate[i].email,
-        mobile: customerstate[i].mobile,
-      });
-    }
-  }
+
+  const data1 = customerstate
+    .filter((customer) => customer.role !== "admin")
+    .map((customer, index) => ({
+      key: index + 1,
+      name: `${customer.firstname} ${customer.lastname}`,
+      email: customer.email,
+      mobile: customer.mobile,
+    }));
+
+  // Hàm xuất dữ liệu ra file Excel
+  const exportToExcel = () => {
+    // Tạo worksheet từ dữ liệu
+    const worksheet = XLSX.utils.json_to_sheet(data1);
+    // Tạo workbook và thêm worksheet vào
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Customers");
+
+    // Xuất workbook ra buffer
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+
+    // Tạo Blob từ buffer
+    const data = new Blob([excelBuffer], { type: "application/octet-stream" });
+    // Lưu file Excel
+    saveAs(data, "danh_sach_khach_hang.xlsx");
+  };
 
   return (
     <div>
-      <h3 className="mb-4 title">Customers</h3>
+      <h3 className="mb-4 title">Danh sách khách hàng</h3>
+      <div style={{ marginBottom: 16 }}>
+        <button type="primary" onClick={exportToExcel}>
+          Xuất ra Excel
+        </button>
+      </div>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
